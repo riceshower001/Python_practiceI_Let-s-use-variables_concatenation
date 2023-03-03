7a723fae46d26f3fe8492acecfba0a5c7a10d416
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-03-03 17:12:40 +0900

    Add files via upload

---
+++ Python_practiceI_Let-s-use-variables_concatenation.py
@@ -0,0 +1,5 @@
+# Substitute the string "◆◆◆ ◆◆◆" in the variable "my_name".
+my_name = "◆◆◆ ◆◆◆"
+
+# Use "my_name" to concatenate the variable and the string and output it so that "I am ◆◆◆ ◆◆◆."
+print("I am "+my_name+".")
